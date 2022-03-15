94f420ecbe0431710bd431af7158bbfa2ebd3295
Fadil1212 <99661887+Fadil1212@users.noreply.github.com>
2022-03-16 01:15:55 +0530

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+#include <stdio.h>
+ /**
+ *write - function to print string
+ * @filedes - file descriptor
+ * @buf - pointer to array of character
+ * @nbyte - number of bytes to print
+ * Return: 0 sucess
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
+/**
+* main - entry point
+*
+* Return: 0 success
+*/
 int main(void)
 {
-        char *sh = "_putchar";
-
-        while (*sh)
-        {
-                _putchar(*sh);
-                sh++;
-        }
-        _putchar('\n');
-
-        return (0);
+write(1, "_putchar\n", 9);
+return (0);
 }
