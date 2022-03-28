2d443a00ef49063231b23a2f99a28f5e25b42b2d
Fadil1212 <fadilmohammed208@gmail.com>
2022-03-28 12:55:09 +0530

    commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,22 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_chessboard - prints 2D array of chessboard
- * @a: chessboard array
+ * *print_chessboard - description
+ * @a: board
  * Return: void
  */
 
+
 void print_chessboard(char (*a)[8])
 {
-	int j = 1;
+int i, j;
 
-	while (a[0][j - 1] != 0)
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		_putchar(a[0][j - 1]);
-		if (j > 1 && j % 8 == 0)
-			_putchar('\n');
-		j++;
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
+
 }
