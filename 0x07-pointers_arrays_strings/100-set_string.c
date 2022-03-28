97f286b8702484cac6527e562dced26a38453eb3
Fadil1212 <fadilmohammed208@gmail.com>
2022-03-28 16:27:51 +0530

    commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
+ *
+ * @s: The pointer to change
+ * @to: The value to change the pointer to
+ *
+ * Return: nothing
  */
 
 void set_string(char **s, char *to)
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,27 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * *print_chessboard - description
- * @a: board
- * Return: void
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
  */
-
 
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i < 8; i++)
-{
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i][j]);
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
+		}
 	}
-	_putchar('\n');
-}
-
 }
