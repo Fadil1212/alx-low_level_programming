610099845475ce7cc2c66725e629dc1a92eb594e
Fadil1212 <fadilmohammed208@gmail.com>
2022-04-01 11:37:09 +0530

    hello

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,33 +1,16 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * print_diagsums - prints the sum of the two diagonals of a square
- * matrix of integers
- *
- * @a: the name of the array
- * @size: the size of the array
- *
- * Return: nothing
- */
 
-void print_diagsums(int *a, int size)
+#include<stdio.h>
+int main()
 {
-	int i = 0;
-	int j = size - 1;
-	int sum1 = 0;
-	int sum2 = 0;
-
-	while (i <= (size * size))
-	{
-		sum1 = sum1 + a[i];
-		i = i + size + 1;
-	}
-
-	while (j < (size * size - 1))
-	{
-		sum2 += a[j];
-		j = j + size - 1;
-	}
-
-	printf("%d, %d\n", sum1, sum2);
+int n, rev=0,rem; 
+printf("enter the number:");
+scanf("%d",n);
+while (n!=0)
+{
+rem=n % 10;
+rev=rev * 10 + rem;
+n /= 10;
+}
+printf("the reversed number is %d ",rev);
+return 0;
 }
